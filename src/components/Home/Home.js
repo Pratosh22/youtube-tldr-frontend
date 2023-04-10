cdb2bdc94217f4c59ea3381c88a9f801773c8651
Shelter <pradosh.balaji@gmail.com>
2023-04-10 20:07:30 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,25 @@
+import { useRef, useEffect } from "react";
 import "./Home.css";
+import Typed from "typed.js";
 import Form from "../Form/Form";
-import { useTypeWriter } from "@vegadev/react-type-writer";
 function Home() {
-  const text = useTypeWriter({
-    text: [
-      "Summarize ",
-      "Explain ",
-    ],
-    infiniteLoop: true,
-    delay: 1000,
-    blinker: '',
-  });
+  const el = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Summarize", "Explain"],
+      typeSpeed: 100,
+      backSpeed: 40,
+      showCursor: false,
+      loop: true,
+    });
+  }, []);
   return (
     <>
       <header>
         <center>
           <h1>
-            <span>{text}</span> 
-            
-             any video <br></br>in a click.
+            <span ref={el} className="typed"></span> any video <br></br>in a
+            click.
           </h1>
         </center>
       </header>
