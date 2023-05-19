a52cf5cdda9349e5cbccb3a5beb7dc1febc83e27
Shelter <pradosh.balaji@gmail.com>
2023-05-19 18:28:10 +0530

    Added analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import { Analytics } from '@vercel/analytics/react';
 import "./App.css";
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <Footer />
+      <Analytics/>
     </>
   );
 }
